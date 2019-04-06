30ae14e9539a21c412d0e7b4e625690d9e482a46
SBC-VN <vnestohr@juno.com>
2019-04-05 22:37:01 -0400

    button click handlers.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -20,4 +20,22 @@ $("#usernotify-button").on("click",function(event) {
   modelDiv.css("display","none");
 });
 
-    //getCurrentLocation();
+$("#bucket-list-table").on("mouseenter",".wish-item-name",function(event) {
+  this.style.background = "lightgreen";
+});
+
+$("#bucket-list-table").on("mouseleave",".wish-item-name",function() {
+  this.style.background = "none";
+});
+
+$("#bucket-list-table").on("click",".wish-item-name",function() {
+  populateItemDataView(parseInt(this.getAttribute("wish-item-index")));
+});
+
+$("#bucket-list-table").on("click","btn",function() {
+  console.log("click button",this.getAttribute("wish-item-index"));
+});
+
+function populateItemDataView(index) {
+  
+}
